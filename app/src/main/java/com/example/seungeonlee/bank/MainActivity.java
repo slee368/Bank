b602ff900ea252a2a48131825db75a64406dd160
slee368 <slee368@jhu.edu>
2016-02-23 00:04:02 -0500

    fuk u david

--- app/src/main/java/com/example/seungeonlee/bank/MainActivity.java
+++ app/src/main/java/com/example/seungeonlee/bank/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.seungeonlee.bank;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,15 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                addTransaction();
             }
         });
     }
@@ -48,5 +46,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addTransaction(){
+        Intent intent = new Intent(MainActivity.this, TransactionActivity.class);
+        startActivity(intent);
     }
 }
